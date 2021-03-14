94d2d3af91034d1f16d2e9710e6b5ae369da5a12
Reinaldo Dias Junior <junior@Reinaldos-MacBook-Pro.local>
2021-03-14 01:17:37 -0300

    Adds GET /users/userId

--- pages/api/v1/users/[userId].ts
+++ pages/api/v1/users/[userId].ts
@@ -4,17 +4,25 @@ import "../../../../db/db.ts";
 import User from "../../../../models/user.model";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name, email } = req.body;
+  if (req.method === "GET") {
+    const { userId: id } = req.query;
 
-  const userAlreadyExists = await User.findOne({ email });
+    if (id.length != 24) {
+      return res
+        .status(400)
+        .json({ error: "user id must be 24 characters long" });
+    }
 
-  if (userAlreadyExists) {
-    return res.status(400).json({ error: "User already exists" });
+    try {
+      const user = await User.findById(id);
+
+      if (!user) {
+        return res.status(400).json({ error: "no user found" });
+      }
+
+      return res.status(200).json(user);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   }
-
-  const user = new User({ name, email });
-
-  await user.save();
-
-  res.status(200).json(user);
 };
