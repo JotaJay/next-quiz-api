acaa1d3c683287fe9db5915cf80ee21eb3fe6a42
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-03-20 19:17:26 -0300

    creates GET /quizzes/category

---
+++ pages/api/v1/quizzes/category/[category].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+import "../../../../../db/db.ts";
+import Quiz from "../../../../../models/quiz.model";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const { category } = req.query;
+
+      const quiz = await Quiz.find({ category }).exec();
+      console.log("subject", quiz);
+
+      return res.status(200).json(quiz);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+};
