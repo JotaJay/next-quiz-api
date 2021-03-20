c446b9290b87abf86ddb2162281ec71c6a6371c9
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-03-20 19:09:50 -0300

    Adds GET /quizzes/subject

---
+++ pages/api/v1/quizzes/subject/[subject].ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+import "../../../../../db/db.ts";
+import Quiz from "../../../../../models/quiz.model";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const { subject } = req.query;
+
+      const quiz = await Quiz.find({ subject }).exec();
+      console.log("subject", quiz);
+
+      if (!quiz) {
+        return res.status(400).json({ msg: "No quiz found" });
+      }
+
+      return res.status(200).json(quiz);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+};
